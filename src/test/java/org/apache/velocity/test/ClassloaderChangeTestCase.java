c224bdf3dc5c11075bb5eec13f0b9d0eb28c5a7d
Frederick N. Brier <fbrier@multideck.com>
2014-09-21 16:57:05 -0700

    Moved TestClassloader into its own java file. Was causing test plugin to die.
    When this was fixed, over 30 errors and 20 or so failures in unit tests
    that had not been being run showed up.  Ant apparently stopped running
    the tests, but still showed success.  For instance, the MultiLoaderTestCase
    should not pass as written.  When using ant to build the original project,
    the MultiLoaderTestCase surefire results do not exist in the test-reports
    directory.

--- src/test/java/org/apache/velocity/test/ClassloaderChangeTestCase.java
+++ src/test/java/org/apache/velocity/test/ClassloaderChangeTestCase.java
@@ -19,9 +19,6 @@ package org.apache.velocity.test;
  * under the License.    
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.StringWriter;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -152,33 +149,3 @@ public class ClassloaderChangeTestCase extends TestCase
 
 }
 
-/**
- *  Simple (real simple...) classloader that depends
- *  on a Foo.class being located in the classloader
- *  directory under test
- */
-class TestClassloader extends ClassLoader
-{
-    private final static String testclass = "/classloader/Foo.class";
-
-    private Class fooClass = null;
-
-    public TestClassloader() throws Exception
-    {
-        File f = new File( getClass().getResource( testclass ).toURI() );
-
-        byte[] barr = new byte[ (int) f.length() ];
-
-        InputStream inputStream = new FileInputStream( f );
-        inputStream.read( barr );
-        inputStream.close();
-
-        fooClass = defineClass("Foo", barr, 0, barr.length);
-    }
-
-
-    public Class findClass(String name)
-    {
-        return fooClass;
-    }
-}
